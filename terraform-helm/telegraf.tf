90a49b90d8a6652e46f6529a58bfca07a3d956b5
Oleg Blazhyievskyi <OBlazhyievskyi@corelogic.com>
2020-08-21 17:00:55 +0200

    add telegraf

--- terraform-helm/telegraf.tf
+++ terraform-helm/telegraf.tf
@@ -1,12 +1,13 @@
 # # === Telegraf ===
-# resource "helm_release" "telegraf" {
-#   name  = "telegraf"
-#   repository = "influxdata"
-#   chart      = "influxdata/telegraf"
-#   version    = "1.7.21"
-#   # namespace = var.namespace
-
-#   # provisioner "local-exec" {
-#   #   command = "helm test telegraf"
-#   # }
-# }
+resource "helm_release" "telegraf" {
+  name  = "telegraf"
+  repository = "influxdata"
+  chart      = "influxdata/telegraf"
+  version    = "1.7.21"
+  values = [
+    "${file("${path.module}/telegraf-values.yml")}"
+  ]
+  # provisioner "local-exec" {
+  #   command = "helm test telegraf"
+  # }
+}
